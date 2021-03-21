3242bc9050a33b7250e5b7357aa6b8c878a273af
Tony Perri <tony.perri@progress.com>
2021-03-21 18:48:57 -0400

    Added module manifest

--- MOVEit.MIA/MOVEit.MIA.psm1
+++ MOVEit.MIA/MOVEit.MIA.psm1
@@ -17,7 +17,3 @@ Set-Alias -Name Get-MIAFileActivity -Value Get-MIAReportFileActivity
 Set-Alias -Name Get-MIAAudit -Value Get-MIAReportAudit
 
 Export-ModuleMember -Function $PublicFunctions.Basename -Alias 'Get-MIA*'
-
-# Update the format data to display the Log output and paging info   
-Update-FormatData -AppendPath "$PSScriptRoot\Format\MIA.Format.ps1xml" 
-Update-FormatData -AppendPath "$PSScriptRoot\Format\MIALog.Format.ps1xml"
