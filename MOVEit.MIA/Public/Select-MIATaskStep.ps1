1480ae07669feec794beaae9bbce0956b8e46bee
Tony Perri <tony.perri@progress.com>
2023-10-25 20:24:18 -0400

    Add Select-MIATaskStep function

---
+++ MOVEit.MIA/Public/Select-MIATaskStep.ps1
@@ -0,0 +1,38 @@
+function Select-MiATaskStep {
+    <#
+        .SYNOPSIS
+        Select type(s) of steps from anywhere in a task
+        .NOTES
+        Recursive
+        Most useful for Advanced tasks where steps are nested
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [psobject]$InputObject,
+        
+        [Parameter()]
+        [ValidateSet('Source', 'Process', 'Destination', 'NextAction', 
+                     'Email', 'Comment', 'For', 'If', 'RunTask', 'UpdOrig')]
+        [string[]]$StepType = @('Source', 'Destination')
+    )
+
+    begin { 
+        # StepTypes that contain other stepTypes and should be recursed.       
+        $containerName = 'steps', 'For', 'If', 'When', 'Otherwise', 'NextActions'
+    }
+
+    process {        
+        $InputObject.PSObject.Properties | ForEach-Object {
+            if ($_.Name -in $StepType) {
+                # This is a property we are looking for
+                [pscustomobject]@{$_.Name = $_.Value}                
+            }
+            
+            if ($_.Name -in $containerName) {
+                # Recurse
+                $_.Value | Select-MiATaskStep -StepType $StepType
+            }
+        }
+    }
+}
