0de6d79cca640f5c7bf3e633ce596774f7cbf018
Tony Perri <tony.perri@progress.com>
2023-10-25 21:11:03 -0400

    Add Get-MIAStandardScript

---
+++ MOVEit.MIA/Public/Get-MIAStandardScript.ps1
@@ -0,0 +1,62 @@
+function Get-MIAStandardScript {
+    [CmdletBinding(DefaultParameterSetName='List')]
+    param (
+        [Parameter(Mandatory,
+                    ParameterSetName='Detail')]
+        [string]$ScriptId,
+
+        [Parameter(Mandatory=$false,
+                    ParameterSetName='List')]
+        [string]$Name,
+
+        [Parameter(Mandatory=$false,
+                    ParameterSetName='List')]
+        [ValidateSet('ID','Name','Description','Source',
+                    'Lang', IgnoreCase = $false)]
+        [string[]]$Fields,
+
+        [Parameter(Mandatory=$false,
+                    ParameterSetName='List')]
+        [int32]$Page,
+
+        [Parameter(Mandatory=$false,
+                    ParameterSetName='List')]
+        [int32]$PerPage,
+
+        [Parameter(Mandatory=$false,
+                    ParameterSetName='List')]                         
+        [switch]$IncludePaging,
+
+        # Context
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Context = $script:DEFAULT_CONTEXT
+    )
+    
+    try {   
+        # Set the resource for this request
+        $resource = "standardscripts"
+        
+        # Send the request and write the response
+        switch ($PSCmdlet.ParameterSetName) {
+            'Detail' {
+                Invoke-MIARequest -Resource "$resource/$ScriptId" -Context $Context |
+                    Write-MIAResponse -TypeName 'MIAStandardScript'
+            }
+            'List' {
+                $query = @{}
+                switch ($PSBoundParameters.Keys) {
+                    Name { $query['name'] = $Name }
+                    Fields { $query['fields'] = $Fields -join ',' }
+                    Page { $query['page'] = $Page }
+                    PerPage { $query['perPage'] = $PerPage }
+                }
+                Invoke-MIARequest -Resource "$resource" -Context $Context -Body $query |
+                    Write-MIAResponse -Typename "MIAStandardScript" -IncludePaging:$IncludePaging
+            }
+        }
+    }
+    catch {
+        $PSCmdLet.ThrowTerminatingError($PSItem)
+    }
+}
