e42b48e2ea6f48cddfad1b67d7e73c2bb47e69e2
Tony Perri <tony.perri@progress.com>
2023-11-07 12:32:14 -0500

    Change from recursive to iterative

--- MOVEit.MIA/Public/Select-MIATaskStep.ps1
+++ MOVEit.MIA/Public/Select-MIATaskStep.ps1
@@ -3,13 +3,13 @@ function Select-MiATaskStep {
         .SYNOPSIS
         Select type(s) of steps from anywhere in a task
         .NOTES
-        Recursive
         Most useful for Advanced tasks where steps are nested
     #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory,ValueFromPipeline)]
-        [psobject]$InputObject,
+        [PSTypeName('MOVEit.MIA.Task')]
+        [psobject]$Task,
         
         [Parameter()]
         [ValidateSet('Source', 'Process', 'Destination', 'NextAction', 
@@ -22,16 +22,21 @@ function Select-MiATaskStep {
         $containerName = 'steps', 'For', 'If', 'When', 'Otherwise', 'NextActions'
     }
 
-    process {        
-        $InputObject.PSObject.Properties | ForEach-Object {
-            if ($_.Name -in $StepType) {
-                # This is a property we are looking for
-                [pscustomobject]@{$_.Name = $_.Value}                
-            }
-            
-            if ($_.Name -in $containerName) {
-                # Recurse
-                $_.Value | Select-MiATaskStep -StepType $StepType
+    process {
+        # Initialize the array with this object, but it will grow as nested objects are appended to the array
+        $objArray = @($Task)
+
+        for ($i = 0; $i -lt $objArray.Count; $i++) {
+            $objArray[$i].PSObject.Properties | ForEach-Object {
+                if ($_.Name -in $StepType) {
+                    # This is a property we are looking for
+                    [pscustomobject]@{$_.Name = $_.Value}                
+                }
+                
+                if ($_.Name -in $containerName) {
+                    # Append this object array to the end so these get processed too.
+                    $objArray += @($_.Value)
+                }
             }
         }
     }
